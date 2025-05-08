5f4258948959ee376fd504d301cb8289d3d1e362
Tomas-coder-dev <aylasmorenof@gmail.com>
2025-05-08 18:05:11 -0500

    Update

--- src/services/quotes/quotes.d.ts
+++ src/services/quotes/quotes.d.ts
@@ -1,14 +1,11 @@
 export interface QuoteProps {
-    id: string | number;
-    quoteNumber: string;
-    client: string;
-    date: string;
-    validUntil: string;
-    status: 'draft' | 'sent' | 'approved' | 'rejected';
-    items: Array<{
-      productId: string;
-      quantity: number;
-      unitPrice: number;
-    }>;
-    total: number;
-  }
+  id: string | number;
+  quoteNumber: string;           // Código cotización
+  ruc: string;                   // RUC cliente
+  razonSocial: string;           // Razón social cliente
+  departamento: string;          // Departamento
+  plazaEntrega: string;          // Plaza de Entrega
+  date: string;                  // Fecha cotización
+  status: 'draft' | 'sent' | 'approved' | 'rejected'; // Estado
+  total: number;                 // Monto
+}
